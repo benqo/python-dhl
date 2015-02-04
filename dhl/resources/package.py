b3863b19eec6fe0ade621e1d8d2ad2a114a20f5f
Benjamin Polovic <benkoman@gmail.com>
2015-02-04 16:14:57 +0100

    Added price and description to packages.

--- dhl/resources/package.py
+++ dhl/resources/package.py
@@ -3,8 +3,10 @@ class DHLPackage:
     A class for creating separate packages contained in a shipment.
     """
 
-    def __init__(self, weight, length, width, height):
+    def __init__(self, weight, length, width, height, price=None, description=None):
         self.weight = weight  # kg
         self.length = length  # cm
         self.width = width  # cm
         self.height = height  # cm
+        self.price = price
+        self.description = description
