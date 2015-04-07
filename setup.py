24a130549fcfa1885e669c59b741b00969a9d500
Benjamin Polovic <benkoman@gmail.com>
2015-04-07 10:30:35 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ It also provides the ability to save the shipping labels.
 setup(
     name='python-dhl',
 
-    version='1.0.0.dev8',
+    version='1.0.0.dev9',
 
     description='Python package for DHL webservice',
     long_description=long_description,
